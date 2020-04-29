e1c7cdc09e2c1e58149576a2a097dee37355bfb8
GrizelGonzalez <miriamgonzalez859@gmail.com>
2020-04-28 21:01:18 -0500

    arrays I

---
+++ src/module_03/arrays_I.js
@@ -0,0 +1,17 @@
+/*buscar la posición del número en el Array*/
+
+let numeros=[1,2,3,4,5,6];
+ 
+console.log(numeros[1]);/*aquí estamos buscando la posicón del número 1
+que se encuentra en el arreglo*/
+
+/*sumar dos números del arreglo*/
+
+console.log(numeros[0]+numeros[1]);
+
+/*Arrays con strings*/
+let palabras=['Miriam', 'Grizel', 'Gonzalez'];
+
+console.log(palabras[2]); //buscar la posicion del string
+console.log(palabras[2].length); //buscar la cantidad de letras que contiene el string
+console.log(`La palabra ${palabras[2]} tiene ${palabras[2].length} letras`);
