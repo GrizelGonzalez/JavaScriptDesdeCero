e267a5d600d86a91207f71bd5841a0b04bb9e63e
GrizelGonzalez <miriamgonzalez859@gmail.com>
2020-04-29 22:12:18 -0500

    bucles for, while y do while

---
+++ src/module_03/bucle_for.js
@@ -0,0 +1,18 @@
+
+/*for(let i=0; i<=10; i++){
+    console.log(i);
+    
+}*/
+
+ let numbers = [0,1,2,3,4,5]
+
+ /*for(let i=0; i<numbers.length; i++){
+     console.log(numbers[i]);
+     
+ }*/
+
+ for(let i=0; i<numbers.length; i++){
+     console.log(`i vale ${i} y el valor de esa posición en el 
+     array es ${numbers[i]}`);
+     
+ }
---
+++ src/module_03/bucle_while_do_while.js
@@ -0,0 +1,11 @@
+
+let pass = '';
+
+while(pass != 'hola'){ 
+    pass = prompt('Introduzca su contraseña')
+}
+console.log('Fin del bucle');
+
+do{
+    pass = prompt('Introduzca su contraseña')
+}while(pass != 'hola');
