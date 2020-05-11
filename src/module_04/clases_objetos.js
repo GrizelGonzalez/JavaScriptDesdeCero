6253a92c73a93a96a066a95e124a6dd62f91b1f1
GrizelGonzalez <miriamgonzalez859@gmail.com>
2020-05-11 17:16:07 -0500

    Co-authored-by: José Ignacio Menchaca Castro <ignaciomenchaca666@gmail.com>

--- src/module_04/clases_objetos.js
+++ src/module_04/clases_objetos.js
@@ -1,36 +1,40 @@
-class Book{
-    constructor(titulo,autor,anio,genero){
-        this.titulo = titulo
-        this.autor = autor
-        this.anio = anio
-        this.genero = genero
-    }
-    bookInfo(){
-        return `${this.titulo} es un libro de ${this.genero} 
-        escrito por ${this.autor} en el año ${this.anio}`
-    }
+class Book {
+  constructor(titulo, autor, anio, genero) {
+    this.titulo = titulo;
+    this.autor = autor;
+    this.anio = anio;
+    this.genero = genero;
+  }
+
+  bookInfo() {
+    return `${this.titulo} es un libro de ${this.genero} 
+        escrito por ${this.autor} en el año ${this.anio}`;
+  }
 }
 
-let books = []
+let books = [];
 
-while(books.length < 3){
-    let titulo = prompt('Introduzca el titulo del libro')
-    let autor = prompt('Introduzca el autor del libro')
-    let anio = prompt('Indroduzca el año del libro')
-    let genero = prompt('Intoduzca el género del libro').toLowerCase
+while (books.length < 2) {
+    
+  let titulo = prompt("Introduzca el titulo del libro");
+  let autor = prompt("Introduzca el autor del libro");
+  let anio = prompt("Indroduzca el año del libro");
+  let genero = prompt("Intoduzca el género del libro").toLowerCase();
 
-    if(titulo != '' && autor != '' 
-    && !isNaN(anio) && anio.length == 4 && (genero == 'aventura' 
-    || genero == 'teror' || genero == 'fantasia')){
-
-        books.push(new Book(titulo, autor, anio, genero))
-    }
+  if (
+    titulo != "" &&
+    autor != "" &&
+    !isNaN(anio) &&
+    anio.length == 4 &&
+    (genero == "aventura" || genero == "terror" || genero == "fantasia")
+  ) 
+  {
+    books.push(new Book(titulo, autor, anio, genero));
+  }
 }
 
 const showAllBooks = () => {
-    console.log(books);
-}
-showAllBooks()
+  console.log(books);
+};
 
-
-
+showAllBooks();
