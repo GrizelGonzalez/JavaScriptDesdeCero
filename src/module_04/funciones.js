925784cbe0ecae031bd56a2522bc98835820287f
GrizelGonzalez <miriamgonzalez859@gmail.com>
2020-05-11 13:14:01 -0500

    funciones

---
+++ src/module_04/funciones.js
@@ -0,0 +1,11 @@
+const saludarUsuario = (user) => 
+console.log(`Hola ${user}`);
+
+saludarUsuario('Pepe')
+
+
+const suma = (num1,num2) => num1+num2
+
+let result = suma(3,6)
+
+console.log(result);
