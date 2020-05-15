6949a17b7ec66384c62108a86247181caeed27c2
GrizelGonzalez <miriamgonzalez859@gmail.com>
2020-05-15 10:46:54 -0500

    metodos array terminado

--- src/module_04/metodos_arrays.js
+++ src/module_04/metodos_arrays.js
@@ -27,3 +27,15 @@ console.log(palabra.every(palabra => palabra.length>2));
 /*.map(callback) transforma todos los elementos del array y devuelve
 un nuevo array*/
 
+const number = [12, 25, 47, 84, 98]
+const numero = number.map(number => number * 2)
+console.log(numero);
+
+/* .filter toma los numeros mayores a los que le indiques*/
+
+const number2 = number.filter(number => number >80)
+console.log(number2);
+
+/*.reduce se junta los números del arreglo*/
+
+console.log(number.reduce((a,b) => a+b));
