46d7937b5106867f0cc16b047db2efcb2295ce57
GrizelGonzalez <miriamgonzalez859@gmail.com>
2020-05-19 17:59:13 -0500

    Co-authored-by: José Ignacio Menchaca Castro <ignaciomenchaca666@gmail.com>

--- src/module_04/fetch_post.js
+++ src/module_04/fetch_post.js
@@ -2,10 +2,11 @@
 /*if (window.fetch != undefined) console.log('FETCH OK')
 else console.log('FETCH NOW WORKS')*/
 
-const button = document.getElementById('button')
+const button = document.getElementById('button');
 
 //res = response = respuesta
 button.addEventListener('click', () => {
+    console.log(button);
     /*fetch('https://jsonplaceholder.typicode.com/users')
     .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
     .then(res => res.json())
