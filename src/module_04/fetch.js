38113e2fcd24eea4ef7abb6c24e5d7a075b16066
GrizelGonzalez <miriamgonzalez859@gmail.com>
2020-05-19 12:33:01 -0500

    Fetch sin terminar

---
+++ src/module_04/fetch.js
@@ -0,0 +1,13 @@
+//Comprobación de soporte FECHT
+if (window.fetch != undefined) console.log('FETCH OK')
+else console.log('FETCH NOW WORKS')
+
+//const button = document.getElementById('button')
+
+//res = response = respuesta
+/*button.addEventListener('click', () => {
+    fetch('https://jsonplaceholder.typicode.com/users')
+    .then(res => res.ok ? Promise.resolve(res) : Promise.reject(res))
+    .then(res => res.json())
+    .then(res => console.log(res))
+});*/
