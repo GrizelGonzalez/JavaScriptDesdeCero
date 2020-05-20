2b390265013cc8ff632241aa7254b7bd80cdd1c8
GrizelGonzalez <miriamgonzalez859@gmail.com>
2020-05-20 17:56:06 -0500

    fetch por url

--- src/module_04/fetch3.js
+++ src/module_04/fetch3.js
@@ -5,7 +5,7 @@ const buttonImg = document.getElementById("button-img");
 const buttonPDF = document.getElementById("button-pdf");
 
 buttonImg.addEventListener("click", () => {
-  fetch("mario.jpg", {})
+  fetch("https://upload.wikimedia.org/wikipedia/commons/thumb/b/b6/Image_created_with_a_mobile_phone.png/800px-Image_created_with_a_mobile_phone.png", {})
     .then((res) => res.blob())
     .then((img) => {
       document.querySelector("img").src = URL.createObjectURL(img);
