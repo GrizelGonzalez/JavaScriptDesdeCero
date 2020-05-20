7803ad64e287d8aa2f4a69e35485f8b964340a41
José Ignacio Menchaca Castro <31707457+IgnacioCastro0713@users.noreply.github.com>
2020-05-20 17:18:04 -0500

    Update fetch3.js

--- src/module_04/fetch3.js
+++ src/module_04/fetch3.js
@@ -4,19 +4,10 @@
 const buttonImg = document.getElementById("button-img");
 const buttonPDF = document.getElementById("button-pdf");
 
-const header = new Headers({
-    'Access-Control-Allow-Origin':'*',
-    'Content-Type': 'multipart/form-data'
-})
-
 buttonImg.addEventListener("click", () => {
-  console.log("Aqui", window.location.origin);
-  fetch('mario.jpg', {
-    mode: 'no-cors',
-    header,
-  })
-    .then(res => res.blob())
-    .then(img => {
+  fetch("mario.jpg", {})
+    .then((res) => res.blob())
+    .then((img) => {
       document.querySelector("img").src = URL.createObjectURL(img);
     });
 });
